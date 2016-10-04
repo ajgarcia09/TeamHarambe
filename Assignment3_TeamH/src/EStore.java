64616b51dc069b4cea7905b02e69a0cad0a0635d
ajgarcia09 <AnaJacqueline@WINDOWS-RBUFAVU>
2016-10-04 11:45:14 -0600

    aded print statement2

--- Assignment3_TeamH/src/EStore.java
+++ Assignment3_TeamH/src/EStore.java
@@ -3,5 +3,6 @@ public class EStore {
 	public static void main(String[]args){
 		System.out.println("Hello World");
 		System.out.println("I like Quizno's");
+		System.out.println("Pleas work");
 	}
 }
