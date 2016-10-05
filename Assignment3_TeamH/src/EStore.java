29e70ff029a82db486db5051b3120165d5af1624
ralva <ralva@172.19.0.36>
2016-10-04 18:02:42 -0600

    Added a couple of print statements

--- Assignment3_TeamH/src/EStore.java
+++ Assignment3_TeamH/src/EStore.java
@@ -2,6 +2,9 @@
 public class EStore {
 	public static void main(String[]args){
 		System.out.println("Hello World");
+		System.out.println("This method is on fire");
+		System.out.println("Raise your glass. P!nk");
+		System.out.println("Harambe does not approve");
 	}
 }
 // hello poochy 
