c826bdc8d3700ea8aa93e3268ad7eec4a49f48c4
ajgarcia09 <AnaJacqueline@WINDOWS-RBUFAVU>
2016-10-09 13:35:27 -0600

    Class implementation of setting up reminders and alarms on the fit band.

---
+++ Assignment3_TeamH/src/RemindersAlarm.java
@@ -0,0 +1,21 @@
+
+public class RemindersAlarm {
+	public String reminder;
+	public boolean alarmOn;
+	
+	public String getReminder() {
+		return reminder;
+	}
+	public boolean isAlarmOn() {
+		return alarmOn;
+	}
+	public void setReminder(String reminder) {
+		this.reminder = reminder;
+	}
+	public void setAlarmOn(boolean alarmOn) {
+		this.alarmOn = alarmOn;
+	}
+	
+	
+
+}
